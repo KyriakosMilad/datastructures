2649e6af77adc0c0705cfa6e0245e1e374310ce7
Kyriakos Milad <contact.kyriakos@gmail.com>
2022-03-14 08:22:41 +0200

    binarysearchtree.go: Create Remove() method to remove values from the tree

--- binarysearchtree/binarysearchtree.go
+++ binarysearchtree/binarysearchtree.go
@@ -1,10 +1,21 @@
 package binarysearchtree
 
+import (
+	"fmt"
+)
+
 type node struct {
 	value int
 	left  *node
 	right *node
 	count int
+}
+
+func (n *node) delete() {
+	n.left = nil
+	n.right = nil
+	n.value = 0
+	n.count = 0
 }
 
 type BinarySearchTree struct {
@@ -41,4 +52,80 @@ func (bst *BinarySearchTree) Insert(val int) {
 			}
 		}
 	}
+}
+
+func (bst *BinarySearchTree) Remove(val int) error {
+	err := fmt.Errorf("no node found with value %v", val)
+	if bst.root == nil {
+		return err
+	}
+
+	parent := bst.root
+	current := bst.root
+	for current != nil {
+		if val < current.value {
+			parent = current
+			current = current.left
+		} else if val > current.value {
+			parent = current
+			current = current.right
+		} else {
+			if current.left != nil && current.right != nil {
+				minParent := current
+				min := current.right
+				for min.left != nil {
+					minParent = min
+					min = min.left
+				}
+				if min.right != nil {
+					minParent.left = min.right
+				} else {
+					if minParent.left == min {
+						minParent.left = nil
+					} else if minParent.right == min {
+						minParent.right = nil
+					}
+				}
+				current.value = min.value
+				current.count = min.count
+				min.delete()
+			} else if current.left == nil && current.right != nil {
+				if parent.right == current {
+					parent.right = current.right
+					current.delete()
+				} else if parent.left == current {
+					parent.left = current.right
+					current.delete()
+				} else {
+					bst.root = current.right
+					current.delete()
+				}
+			} else if current.right == nil && current.left != nil {
+				if parent.right == current {
+					parent.right = current.left
+					current.delete()
+				} else if parent.left == current {
+					parent.left = current.left
+					current.delete()
+				} else {
+					bst.root = current.left
+					current.delete()
+				}
+			} else if current.left == nil && current.right == nil {
+				if current == bst.root {
+					bst.root = nil
+				} else if parent.right == current {
+					parent.right = nil
+					current.delete()
+
+				} else if parent.left == current {
+					parent.left = nil
+					current.delete()
+				}
+			}
+			return nil
+		}
+	}
+
+	return err
 }
--- binarysearchtree/binarysearchtree_test.go
+++ binarysearchtree/binarysearchtree_test.go
@@ -84,3 +84,728 @@ func TestBinarySearchTree_Insert(t *testing.T) {
 		t.Errorf("Insert() = %v, want %v", bst, bstInsertExample)
 	}
 }
+
+func Test_node_delete(t *testing.T) {
+	type fields struct {
+		value int
+		left  *node
+		right *node
+		count int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "test delete node",
+			fields: fields{
+				value: 5,
+				left:  &node{},
+				right: &node{},
+				count: 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &node{
+				value: tt.fields.value,
+				left:  tt.fields.left,
+				right: tt.fields.right,
+				count: tt.fields.count,
+			}
+			n.delete()
+			if !reflect.DeepEqual(n, &node{}) {
+				t.Errorf("node delete() = %v, want %v", n, &node{})
+			}
+		})
+	}
+}
+
+func TestBinarySearchTree_Remove(t *testing.T) {
+	type args struct {
+		val int
+	}
+	tests := []struct {
+		name    string
+		bst     *BinarySearchTree
+		args    args
+		wantErr bool
+		want    *BinarySearchTree
+	}{
+		{
+			//               25
+			//      20               36
+			//   10    22         30       40
+			//  5  12           28      38     48
+			//        14                     46
+			//             will be:
+			//               25
+			//      20                  36
+			//   10                30        40
+			//  5  12           28       38      48
+			//        14                       46
+			name: "remove node with no child (on the right to the parent)",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+			args:    args{val: 22},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			//               25
+			//      20               36
+			//   10    22         30       40
+			//  5  12           28      38     48
+			//        14                     46
+			//             will be:
+			//               25
+			//      20               36
+			//   10    22         30       40
+			//  5  12                   38     48
+			//        14                     46
+			name: "remove node with no child (on the left to the parent)",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+			args:    args{val: 28},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+		},
+		{
+			//               25
+			//      20                36
+			//   10    22         30       40
+			//  5  12           28      38     48
+			//        14                     46
+			//             will be:
+			//               25
+			//      20                 38
+			//   10    22         30        40
+			//  5  12           28             48
+			//        14                     46
+			name: "remove node with two children",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+			args:    args{val: 36},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 38,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+		},
+		{
+			//               25
+			//      20                36
+			//   10    22         30       40
+			//  5  12           28      38     48
+			//        14                     46
+			//             will be:
+			//               25
+			//      20                 36
+			//   10    22         30         40
+			//  5  12           28        38    46
+			//        14
+			name: "remove node with one child on the right and on the left to the parent",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+			args:    args{val: 48},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 46,
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+		},
+		{
+			//               25
+			//      20                36
+			//   10    22         30       40
+			//  5  12           28             48
+			//        14
+			//             will be:
+			//               25
+			//      20                36
+			//   10    22         30       48
+			//  5  12           28
+			//        14
+			name: "remove node with one child on the right and on the right to the parent",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							right: &node{
+								value: 48,
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+			args:    args{val: 40},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{value: 48},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+		},
+		{
+			//               25
+			//      20                36
+			//   10    22         30       40
+			//  5  12           28      38     48
+			//        14                     46
+			//             will be:
+			//               25
+			//      20                36
+			//   10    22         30       40
+			//  5  14           28      38     48
+			//                               46
+			name: "remove node with one child on the left and on the right to the parent",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{
+								value: 12,
+								right: &node{value: 14},
+							},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+			args:    args{val: 12},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+							right: &node{value: 14},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+		},
+		{
+			//               25
+			//      20                36
+			//   10    22         30       40
+			//  5               28      38     48
+			//                               46
+			//             will be:
+			//               25
+			//      20                36
+			//   5     22         30       40
+			//                  28      38     48
+			//                               46
+			name: "remove node with one child on the left and on the left to the parent",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left: &node{
+							value: 10,
+							left:  &node{value: 5},
+						},
+						right: &node{value: 22},
+					},
+				},
+			},
+			args:    args{val: 10},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left:  &node{value: 5},
+						right: &node{value: 22},
+					},
+				},
+			},
+		},
+		{
+			//               25
+			//      20                36
+			//             will be:
+			//               36
+			//      20
+			name: "remove root has two children",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{value: 36},
+					left:  &node{value: 20},
+				},
+			},
+			args:    args{val: 25},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 36,
+					left:  &node{value: 20},
+				},
+			},
+		},
+		{
+			//               25
+			//                        36
+			//             will be:
+			//               36
+			name: "remove root has one child on the right",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{value: 36},
+				},
+			},
+			args:    args{val: 25},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 36,
+				},
+			},
+		},
+		{
+			//               25
+			//      20
+			//             will be:
+			//               20
+			name: "remove root has one child on the left",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					left:  &node{value: 20},
+				},
+			},
+			args:    args{val: 25},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 20,
+				},
+			},
+		},
+		{
+			//               25
+			//             will be:
+			//               nil
+			name: "remove root has no children",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+				},
+			},
+			args:    args{val: 25},
+			wantErr: false,
+			want: &BinarySearchTree{
+				root: nil,
+			},
+		},
+		{
+			name: "remove value doesn't exist",
+			bst: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left:  &node{value: 5},
+						right: &node{value: 22},
+					},
+				},
+			},
+			args:    args{val: 50},
+			wantErr: true,
+			want: &BinarySearchTree{
+				root: &node{
+					value: 25,
+					right: &node{
+						value: 36,
+						left: &node{
+							value: 30,
+							left:  &node{value: 28},
+						},
+						right: &node{
+							value: 40,
+							left:  &node{value: 38},
+							right: &node{
+								value: 48,
+								left:  &node{value: 46},
+							},
+						},
+					},
+					left: &node{
+						value: 20,
+						left:  &node{value: 5},
+						right: &node{value: 22},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.bst.Remove(tt.args.val); ((err != nil) != tt.wantErr) || !reflect.DeepEqual(tt.want, tt.bst) {
+				t.Errorf("Remove() = %#+v, want %#+v, error = %v, wantErr %v", tt.bst, tt.want, err, tt.wantErr)
+			}
+		})
+	}
+}
