2073022e6fb40e83a2221d3b0b97a8447fc9a763
Kyriakos Milad <contact.kyriakos@gmail.com>
2022-03-31 05:59:57 +0200

    maxbinaryheap.go: Create Insert() and bubbleUp() methods to insert element to the heap

--- binaryheap/maxbinaryheap.go
+++ binaryheap/maxbinaryheap.go
@@ -3,3 +3,18 @@ package binaryheap
 type MaxBinaryHeap struct {
 	list []int
 }
+
+func (mbh *MaxBinaryHeap) bubbleUp(idx int) {
+	parentIdx := (idx - 1) / 2
+	if mbh.list[parentIdx] < mbh.list[idx] {
+		temp := mbh.list[idx]
+		mbh.list[idx] = mbh.list[parentIdx]
+		mbh.list[parentIdx] = temp
+		mbh.bubbleUp(parentIdx)
+	}
+}
+
+func (mbh *MaxBinaryHeap) Insert(val int) {
+	mbh.list = append(mbh.list, val)
+	mbh.bubbleUp(len(mbh.list) - 1)
+}
---
+++ binaryheap/maxbinaryheap_test.go
@@ -0,0 +1,88 @@
+package binaryheap
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMaxBinaryHeap_bubbleUp(t *testing.T) {
+	type fields struct {
+		list []int
+	}
+	type args struct {
+		idx int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []int
+	}{
+		{
+			name: "test bubbleUp",
+			fields: fields{
+				list: []int{41, 39, 33, 18, 27, 12, 55},
+			},
+			args: args{
+				idx: 6,
+			},
+			want: []int{55, 39, 41, 18, 27, 12, 33},
+		},
+		{
+			name: "test bubbleUp with no empty child slot",
+			fields: fields{
+				list: []int{41, 39, 33, 18, 27, 12, 20, 55},
+			},
+			args: args{
+				idx: 7,
+			},
+			want: []int{55, 41, 33, 39, 27, 12, 20, 18},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mbh := &MaxBinaryHeap{
+				list: tt.fields.list,
+			}
+			mbh.bubbleUp(tt.args.idx)
+			if !reflect.DeepEqual(mbh.list, tt.want) {
+				t.Errorf("Inser() = %v, want %v", mbh.list, tt.want)
+			}
+		})
+	}
+}
+
+func TestMaxBinaryHeap_Insert(t *testing.T) {
+	type fields struct {
+		list []int
+	}
+	type args struct {
+		val int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []int
+	}{
+		{
+			name:   "test insert",
+			fields: fields{list: []int{41, 39, 33, 18, 27, 12, 20}},
+			args: args{
+				val: 55,
+			},
+			want: []int{55, 41, 33, 39, 27, 12, 20, 18},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mbh := &MaxBinaryHeap{
+				list: tt.fields.list,
+			}
+			mbh.Insert(tt.args.val)
+			if !reflect.DeepEqual(mbh.list, tt.want) {
+				t.Errorf("Inser() = %v, want %v", mbh.list, tt.want)
+			}
+		})
+	}
+}
